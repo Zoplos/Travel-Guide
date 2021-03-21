3401623abd7107cc4b958d7e971b9ac35ebd6550
Zoplos <giannis97z97z@gmail.com>
2021-03-21 18:30:32 +0200

    cleaned up main

--- src/main/Main.java
+++ src/main/Main.java
@@ -38,73 +38,37 @@ public class Main {
 		City city6 = new City("Kyoto","jp",new int[10],new double[2]);
 		city6.RetrieveData(appid);
 		
-		for(int i=0;i<2;i++) {
-			System.out.println(city1.getGeodesic_vector()[i]);
-		}
-		
-		for(int i=0;i<10;i++) {
-			if(i != 9) {
-				System.out.print(city1.getTerms_vector()[i] + ",");
-			} else {
-				System.out.print(city1.getTerms_vector()[i] + "\n");
-			}
-		}
-				
-		
-	
-		for(int i=0;i<2;i++) {
-			System.out.println(city2.getGeodesic_vector()[i]);
-		}
-		
-		for(int i=0;i<10;i++) {
-			if(i != 9) {
-				System.out.print(city2.getTerms_vector()[i] + ",");
-			} else {
-				System.out.print(city2.getTerms_vector()[i] + "\n");
-			}
-			
-		}
-		
-		
-		MiddleTraveller traveller = new MiddleTraveller("John",123,new int[10],new double[2]);
-		traveller.fillWithData(traveller.getTerms_vector(), traveller.getGeodesic_vector());
-		System.out.println(traveller.calculate_similarity(city1, traveller, 0));
-		System.out.println(traveller.calculate_similarity(city2, traveller, 1));		
-		
-		city1.setSimilarity(traveller.calculate_similarity(city1, traveller, 0.2));
-		city2.setSimilarity(traveller.calculate_similarity(city2, traveller, 0.2));
-		city3.setSimilarity(traveller.calculate_similarity(city3, traveller, 0.2));
-		city4.setSimilarity(traveller.calculate_similarity(city4, traveller, 0.2));
-		city5.setSimilarity(traveller.calculate_similarity(city5, traveller, 0.2));
-		city6.setSimilarity(traveller.calculate_similarity(city6, traveller, 0.2));
-		System.out.println(city1.getSimilarity());
-		System.out.println(city2.getSimilarity());
-		System.out.println(city3.getSimilarity());
-		System.out.println(city4.getSimilarity());
-		System.out.println(city5.getSimilarity());
-		System.out.println(city6.getSimilarity());
-		
-		ArrayList<City> cities = new ArrayList<City>(10);
+		ArrayList<City> cities = new ArrayList<City>();
 		cities.add(city1);
 		cities.add(city2);
 		cities.add(city3);
 		cities.add(city4);
 		cities.add(city5);
 		cities.add(city6);
+							
+		YoungTraveller traveller = new YoungTraveller("John",123,new int[10],new double[2]);
+		traveller.fillWithData(traveller.getTerms_vector(), traveller.getGeodesic_vector());		
+		
+		for(int k = 0; k<cities.size();k++) {
+			cities.get(k).setSimilarity(traveller.calculate_similarity(cities.get(k), traveller, 0.2));
+		}
+				
+		System.out.println("\nSimilarity of each city:\n");
+		for(int k = 0; k<cities.size();k++) {
+			System.out.println(cities.get(k).getSimilarity() + " " +cities.get(k).getName());
+		}				
 		
 		Collections.sort(cities,City.CitySimComparator);
-		System.out.println(cities.get(0).getSimilarity() + " " +cities.get(0).getName());
-		System.out.println(cities.get(1).getSimilarity() + " " +cities.get(1).getName());
-		System.out.println(cities.get(2).getSimilarity() + " " +cities.get(2).getName());
-		System.out.println(cities.get(3).getSimilarity() + " " +cities.get(3).getName());
-		System.out.println(cities.get(4).getSimilarity() + " " +cities.get(4).getName());
-		System.out.println(cities.get(5).getSimilarity() + " " +cities.get(5).getName());
 		
+		System.out.println("\nSorted similarity of each city:\n");
+		for(int k = 0; k<cities.size();k++) {
+			System.out.println(cities.get(k).getSimilarity() + " " +cities.get(k).getName());
+		} 
+				
 		System.out.println(traveller.compare_cities(cities).getName());
-		ArrayList<City> test2 = traveller.compare_cities(cities, 7);
+		ArrayList<City> test2 = traveller.compare_cities(cities, 3);
 		System.out.println(test2.get(0).getName());
-		
-		
+
 	}
 	
 }
